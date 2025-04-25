5a27e7cafb1d34f7e5f7bf20ab111b4ec0c30bc7
Helmer Bergström <hebe2400@student.miun.se>
2025-04-25 12:16:57 +0200

    first commit

---
+++ install.js
@@ -0,0 +1,15 @@
+const { Client } = require("pg");
+
+const client = new Client({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT || 5432,
+    ssl: {
+        rejectUnauthorized: true
+    }
+});
+
+
+module.exports = client;
