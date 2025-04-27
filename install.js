85e0a489110b84608d9ae89645b1e8da0b706bd6
Helmer Bergström <hebe2400@student.miun.se>
2025-04-27 11:26:20 +0200

    Skapar anslutning till databas och tabell "workexperience". Skapar även första routen .get.

--- install.js
+++ install.js
@@ -1,15 +1,45 @@
-const { Client } = require("pg");
+const mysql = require('mysql2');
+require('dotenv').config();
 
-const client = new Client({
+const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    port: process.env.DB_PORT || 5432,
-    ssl: {
-        rejectUnauthorized: true
-    }
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE
 });
 
+connection.connect((error) => {
+    if(error) {
+        console.error("Fel i anslutningen: " + error);
+        return;
+    }
+    console.log("Ansluten till MySQL!");
+});
 
-module.exports = client;
+connection.query("CREATE DATABASE IF NOT EXISTS cv2;", (error, results) => {
+    if(error) throw error;
+
+    console.log("Databas skapad: " + results);
+});
+
+connection.query(` CREATE TABLE IF NOT EXISTS workexperience (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        companyname VARCHAR(200),
+        jobtitle VARCHAR(200),
+        location VARCHAR(200),
+        startdate DATE,
+        enddate DATE,
+        description VARCHAR(300)
+        )`, (error, results) => {
+            if (error) throw error;
+
+            console.log("Table workexperience created: " + results);
+        });
+
+// connection.query("DROP TABLE IF EXISTS workexperience;", (error, results) => {
+//     if(error) throw error;
+
+//     console.log("Table workexperience dropped: " + results);
+// })
+
+module.exports = connection;
--- server.js
+++ server.js
@@ -2,11 +2,22 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
+const db = require('./database');
+
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
+
+app.get('/workexperience', async (req, res) => {
+    try {
+        const [rows] = await db.execute('SELECT * FROM workexperience');
+        res.json(rows);
+    } catch (error) {
+        res.status(500).send("Fel vid hämtning av arbeten: " + error);
+    }
+});
 
 app.listen(port, () => {
     console.log("Server started on: http://localhost:" + port);
