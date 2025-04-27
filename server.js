2ab62695dbab76835e8e91dbc03d4271bbb1c783
Helmer Bergström <hebe2400@student.miun.se>
2025-04-27 12:08:09 +0200

    Skapar ett post-anrop för att lägga till erfarenhet.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
-const db = require('./database');
+const db = require('./install');
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -16,6 +16,26 @@ app.get('/workexperience', async (req, res) => {
         res.json(rows);
     } catch (error) {
         res.status(500).send("Fel vid hämtning av arbeten: " + error);
+    }
+});
+
+app.post('/workexperience', async (req, res) => {
+    const { companyname, jobtitle, location, startdate, enddate, description } = req.body;
+
+    if (!companyname || !jobtitle || !location || !startdate || !enddate || !description) {
+        return res.status(400).send("Alla fält måste fyllas i!");
+    }
+    try {
+        const [result] = await db.execute(
+            `INSERT INTO workexperience (
+                companyname, jobtitle, location, startdate, enddate, description
+            ) VALUES (?, ?, ?, ?, ?, ?)`,
+             [companyname, jobtitle, location, startdate, enddate, description]
+        );
+        res.status(201).send({ message: "Erfarenhet tillagd i databasen!", 
+            id: result.insertId });
+    } catch(error) {
+        res.status(500).send("Fel vid skapande av ny erfarenhet: " + error);
     }
 });
 
