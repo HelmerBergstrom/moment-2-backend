56f96cd8523618cb3b8a7db7f1d0abcc73fb8749
Helmer Bergström <hebe2400@student.miun.se>
2025-04-28 16:22:26 +0200

    Gör om till asynkront och lägger även till resterande parametrar i if-satsen.

--- server.js
+++ server.js
@@ -10,25 +10,30 @@ const port = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
 
-app.get('/workexperience', (req, res) => {
+app.get('/workexperience', async (req, res) => {
     try {
-        const [rows] = db.query('SELECT * FROM workexperience');
+        const [rows] = await db.query('SELECT * FROM workexperience');
         res.json(rows);
     } catch (error) {
         res.status(500).send("Fel vid hämtning av arbeten: " + error);
     }
 });
 
-app.post('/workexperience', (req, res) => {
+app.post('/workexperience', async (req, res) => {
+    console.log(req.body);
+
     let companyname = req.body.companyname;
     let jobtitle = req.body.jobtitle;
     let location = req.body.location;
+    let startdate = req.body.startdate;
+    let enddate = req.body.enddate;
+    let description = req.body.description;
 
-    if(!companyname || !jobtitle || !location) { 
-        return res.status(400).send("Send companyname, jobtitle and location atleast!");
+    if (!companyname || !jobtitle || !location || !startdate || !enddate || !description) { 
+        return res.status(400).send("Send companyname, jobtitle, location, startdate, enddate, and description!");
     }
     try {
-        const [result] = db.query(
+        const [result] = await db.query(
             `INSERT INTO workexperience (
                 companyname, jobtitle, location, startdate, enddate, description
             ) VALUES (?, ?, ?, ?, ?, ?)`,
@@ -39,6 +44,14 @@ app.post('/workexperience', (req, res) => {
     } catch(error) {
         res.status(500).send("Fel vid skapande av ny erfarenhet: " + error);
     }
+});
+
+app.put('/workexperience/:id', async (req, res) => {
+
+});
+
+app.delete('/workexperience/:id', async (req, res) => {
+
 });
 
 app.listen(port, () => {
