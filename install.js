30488ce2b7675dbb00540b8293e7305be368ef26
Helmer Bergström <hebe2400@student.miun.se>
2025-04-28 19:00:16 +0200

    Kommenterar koden.

--- install.js
+++ install.js
@@ -1,6 +1,7 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2'); // Råkade installera mysql2, men testar att köra det ändå.
 require('dotenv').config();
 
+// Skapar koppling till databasen med env-filens variabler.
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
@@ -8,6 +9,7 @@ const connection = mysql.createConnection({
     database: process.env.DB_DATABASE
 });
 
+// Testar anslutningen.
 connection.connect((error) => {
     if(error) {
         console.error("Fel i anslutningen: " + error);
@@ -16,12 +18,14 @@ connection.connect((error) => {
     console.log("Ansluten till MySQL!");
 });
 
+// Skapar databasen om den inte finns. Får namnet cv2.
 connection.query("CREATE DATABASE IF NOT EXISTS cv2;", (error, results) => {
     if(error) throw error;
 
     console.log("Databas skapad: " + results);
 });
 
+// Skapar tabell workexperience om den inte finns.
 connection.query(` CREATE TABLE IF NOT EXISTS workexperience (
         id INT AUTO_INCREMENT PRIMARY KEY,
         companyname VARCHAR(200),
@@ -42,4 +46,5 @@ connection.query(` CREATE TABLE IF NOT EXISTS workexperience (
 //     console.log("Table workexperience dropped: " + results);
 // })
 
+// Exporterar med promise, då det verkar krävas i mysql2.
 module.exports = connection.promise();
--- server.js
+++ server.js
@@ -2,26 +2,32 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
-const db = require('./install');
+const db = require('./install'); // importerar från install.js-filen.
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3001; // port 3001, då 3000 inte fungerar för mig.
 
-app.use(cors());
-app.use(express.json());
+// Middlewares.
+app.use(cors()); // Tillåter korsdomänsförfrågningar.
+app.use(express.json()); // Konverterar JSON-data till JS-objekt.
 
+// Hämtar erfarenheter. Kör async await och try o catch.
 app.get('/workexperience', async (req, res) => {
     try {
-        const [rows] = await db.query('SELECT * FROM workexperience');
-        res.json(rows);
+        const [rows] = await db.query('SELECT * FROM workexperience'); // Lagrar raderna i arrayen rows.
+        // Kontrollerar om arrayen är tom, isåfall returneras en tom array.
+        if(rows.length === 0) {
+            return res.status(200).json([]);
+        }
+        res.json(rows); // Skickar tillbaka raderna i json-format.
     } catch (error) {
-        res.status(500).send("Fel vid hämtning av arbeten: " + error);
+        res.status(500).json({ message: "Fel vid hämtning av arbeten: " + error });
     }
 });
 
+// Lägger till erfarenhet.
 app.post('/workexperience', async (req, res) => {
-    console.log(req.body);
-
+    // Deklarerar variabler för alla parametrar.
     let companyname = req.body.companyname;
     let jobtitle = req.body.jobtitle;
     let location = req.body.location;
@@ -29,32 +35,37 @@ app.post('/workexperience', async (req, res) => {
     let enddate = req.body.enddate;
     let description = req.body.description;
 
+    // if-sats för att kontrollera att allt blivit ifyllt.
     if (!companyname || !jobtitle || !location || !startdate || !enddate || !description) { 
-        return res.status(400).send("Send companyname, jobtitle, location, startdate, enddate, and description!");
+        return res.status(400).json({ message: "Send companyname, jobtitle, location, startdate, enddate, and description!"});
     }
     try {
+        // SQL-fråga för att lägga till i tabellen.
         const [result] = await db.query(
             `INSERT INTO workexperience (
                 companyname, jobtitle, location, startdate, enddate, description
             ) VALUES (?, ?, ?, ?, ?, ?)`,
              [companyname, jobtitle, location, startdate, enddate, description]
         );
-        res.status(201).send({ message: "Erfarenhet tillagd i databasen!", 
+        // Returnerar denna om det fungerar. Status 201 då denna funktion lägger till en ny resurs.
+        res.status(201).json({ message: "Erfarenhet tillagd i databasen!", 
             id: result.insertId });
     } catch(error) {
-        res.status(500).send("Fel vid skapande av ny erfarenhet: " + error);
+        res.status(500).json({ message: "Fel vid skapande av ny erfarenhet: " + error });
     }
 });
 
+// Ändrar en befintlig erfarenhet. Routen körs med "/:id"
 app.put('/workexperience/:id', async (req, res) => {
-    const id = req.params.id;
+    // Deklarerar id som en variabel.
+    const id = req.params.id; 
     const { companyname, jobtitle, location, startdate, enddate, description } = req.body;
 
     if (!companyname || !jobtitle || !location || !startdate || !enddate || !description) { 
-        return res.status(400).send("Send companyname, jobtitle, location, startdate, enddate, and description!");
+        return res.status(400).json({ message: "Send companyname, jobtitle, location, startdate, enddate, and description!"});
     }
 
-    try {
+    try { // SQL-fråga för att uppdatera en erfarenhet.
         const [result] = await db.query(`
             UPDATE workexperience
             SET companyname = ?, jobtitle = ?,
@@ -64,32 +75,33 @@ app.put('/workexperience/:id', async (req, res) => {
             [companyname, jobtitle, location, startdate, enddate, description, id]
         );
         
-        // Påverkas inga rader finns inte ID:t
+        // Påverkas inga rader finns inte ID:t och då skickas felmeddelande ut.
         if(result.affectedRows === 0) {
-            return res.status(404).send("ID hittades inte!")
+            return res.status(404).json({ message: "ID:t hittades inte!"})
         }
-        res.status(200).send({ message: "Erfarenhet uppdaterad!"});
+        res.status(200).json({ message: "Erfarenhet uppdaterad!"});
     } catch(error) {
-        res.status(500).send("Fel vid uppdatering!")
+        res.status(500).json({ message: "Fel vid uppdatering!"})
     }
 });
 
+// Raderar en erfarenhet.
 app.delete('/workexperience/:id', async (req, res) => {
     const id = req.params.id; 
     
-    try {
+    try { // SQL-fråga för att radera en erfarenhet utifrån id:t.
         const [result] = await db.query(`
             DELETE FROM workexperience WHERE id = ?
             `, [id]
         );
 
         if (result.affectedRows === 0) {
-            return res.status(404).send("ID hittades inte!");
+            return res.status(404).json({ message: "ID hittades inte!"});
         }
-        res.status(200).send("Erfarenhet raderad!");
+        res.status(200).json({ message: "Erfarenhet raderad!" });
 
     } catch (error) {
-        res.status(500).send("Fel vid borttagning!");
+        res.status(500).json({ message: "Fel vid borttagning!" });
     }
 });
 
